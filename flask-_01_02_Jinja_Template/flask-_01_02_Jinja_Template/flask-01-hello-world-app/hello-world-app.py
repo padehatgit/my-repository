b1c70f19e28b507b49a0bde568b3f2bdcf89a37b
padehatgit <olapadebabalola.jobber@gmail.com>
2024-03-30 14:22:26 -0400

    kittens

--- flask-_01_02_Jinja_Template/flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ flask-_01_02_Jinja_Template/flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,23 +1,24 @@
-from flask import Flask 
-app = Flask(__name__)
-@app.route('/')
-def hello():
-    return "Hello World"
+# from flask import Flask 
+# app = Flask(__name__)
+# @app.route('/')
+# def hello():
+#     return "Hello World"
 
-@app.route('/second')
-def second():
-    return "This is the second page"
+# @app.route('/second')
+# def second():
+#     return "This is the second page"
 
-@app.route('/third/subthird')
-def third():
-    return "This is the subpage of third page"
-
-
+# @app.route('/third/subthird')
+# def third():
+#     return "This is the subpage of third page"
 
 
 
 
-if __name__ == '__main__':
 
-    app.run(debug=True)
-    # app.run(host= '0.0.0.0', port=8081)
+
+# if __name__ == '__main__':
+
+#     app.run(debug=True)
+#     #app.run(debug=True, port=3000)
+#     # app.run(host= '0.0.0.0', port=8081)
--- flask-_01_02_Jinja_Template/flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
+++ flask-_01_02_Jinja_Template/flask-_01_02_Jinja_Template/flask-02-Jinja_Template/jinja.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
+'''
+Create a function named `head` which sends number `number1`
+and `number2` variables to the `index.html`. 
+Use these variables into the `index.html` file. 
+Assign a URL route the `head` function with decorator `@app.route('/')`.
+'''
 
 
 
@@ -10,5 +15,6 @@ app = Flask(__name__)
 
 
 if __name__== "__main__":
-    app.run(debug=True, port=30000)
+    app.run(debug=True) #This app run by defaul on port=5000
+    #app.run(debug=True, port=3000)
     # app.run(host= '0.0.0.0', port=8081)
