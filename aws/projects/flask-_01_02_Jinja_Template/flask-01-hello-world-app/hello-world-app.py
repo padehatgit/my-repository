9e5c74bcb1ac549d127f5ce9fc6fc7832287731a
padehatgit <olapadebabalola.jobber@gmail.com>
2024-05-26 14:33:37 -0400

    hello world app.py

--- aws/projects/flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
+++ aws/projects/flask-_01_02_Jinja_Template/flask-01-hello-world-app/hello-world-app.py
@@ -1,5 +1,30 @@
+
+#   mport Flask module.
 from flask import Flask 
+
+#   Create an object named `app` from imported Flask module.
 app = Flask(__name__)
+
+#   Assign a URL route to the `hello` function with decorator `@app.route('/')`.
+@app.route('/')
+
+#   Create a function `second` which returns a string `This is the second page` and assign a URL route the `second` function with decorator `@app.route('/second')`. 
+def hello():
+     return "Hello World"
+
+@app.route('/second')
+def second():
+      return "This is second page"
+
+'''
+Create a function `third` which returns a string `This is the subpage of third page` and assign a URL 
+route the `third` function with decorator `@app.route('/third/subthird')`.
+''' 
+@app.route('/forth/<string:my_id>')
+def forte(my_id):
+      return f"The page id is {my_id}"
+
+
 
 
 
@@ -8,5 +33,6 @@ app = Flask(__name__)
 
 if __name__ == '__main__':
 
-    app.run(debug=True)
-    # app.run(host= '0.0.0.0', port=8081)
+       app.run(debug=True)
+       #app.run(debug=True, port=3000)
+       # app.run(host= '0.0.0.0', port=8081)
